98169e7de1a357b1b57985faaf0d00704af40b82
andyrofl <andy@andyrofl.com>
2024-01-25 17:33:11 -0500

    Created program template

---
+++ Program_template.m
@@ -0,0 +1,10 @@
+%Clear the command window and all variables
+clc %Clear the command window contents
+clear %Clear the workspace variables
+
+% Output of the title and author to the command window
+program_name= "Program_";
+name="Andy Pacher";
+assisted_by "Nobody";
+fprintf("Output for %s written by %s, with assitance from %s. \n\n", program_name, name, assisted_by)
+
