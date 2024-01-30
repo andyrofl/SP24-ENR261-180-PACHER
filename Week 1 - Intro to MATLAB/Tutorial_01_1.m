883796d4020a1e3ae61cdc113aa488417eb93808
andyrofl <andy@andyrofl.com>
2024-01-30 17:52:13 -0500

    Create Tutorial_01_1.m

---
+++ Week 1 - Intro to MATLAB/Tutorial_01_1.m
@@ -0,0 +1,22 @@
+%Clear the command window and all variables
+clc %Clear the command window contents
+clear %Clear the workspace variables
+
+% Output of the title and author to the command window
+tutorial_name= "Tutorial_01_1";
+name="Andy Pacher";
+assisted_by = "Nobody";
+fprintf("Output for %s written by %s, with assitance from %s. \n\n", tutorial_name, name, assisted_by)
+
+%add 2 and 3
+addition = 2+3
+%subtract 3 from 2
+subtraction = 2-3
+%multiply 2 by 3
+multiplication = 2*3
+%divide 1 by 2
+division = 1/2
+%show 2 to the power of 3
+exponentiation = 2^3
+
+%end tutorial
