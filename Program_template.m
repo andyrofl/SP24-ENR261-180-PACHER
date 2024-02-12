86d3d6011b21e01ab45fb729b700630c8e138872
andyrofl <andy@andyrofl.com>
2024-02-11 23:46:08 -0500

    Updated workspace and templates

--- Program_template.m
+++ Program_template.m
@@ -3,8 +3,8 @@ clc %Clear the command window contents
 clear %Clear the workspace variables
 
 % Output of the title and author to the command window
-program_name= "Program_";
-name="Andy Pacher";
+program_name = "Program_";
+name = "Andy Pacher";
 assisted_by = "Nobody";
 fprintf("Output for %s written by %s, with assitance from %s. \n\n", program_name, name, assisted_by)
 
--- Tutorial_template.m
+++ Tutorial_template.m
@@ -3,8 +3,8 @@ clc %Clear the command window contents
 clear %Clear the workspace variables
 
 % Output of the title and author to the command window
-tutorial_name= "Tutorial_";
-name="Andy Pacher";
+tutorial_name = "Tutorial_";
+name = "Andy Pacher";
 assisted_by = "Nobody";
 fprintf("Output for %s written by %s, with assitance from %s. \n\n", tutorial_name, name, assisted_by)
 
