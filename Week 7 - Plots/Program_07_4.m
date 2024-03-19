0b9bd4e7399b099c66ae8cd86c28c98872bca454
andy rofl <andy@andyrofl.com>
2024-03-19 18:41:51 -0400

    fixed scale error

--- Week 7 - Plots/Program_07_4.m
+++ Week 7 - Plots/Program_07_4.m
@@ -17,8 +17,7 @@ fprintf("Output for %s written by %s, with assistance from %s. \n\n", program_na
 [X,Y] = meshgrid(-3.5:0.25:3.5);
 
 %calculate the z values
-exponent = -1.5*X*sqrt(X^2 + Y^2);
-Z= 1.8^(exponent)*cos(0.5*Y)*sin(X);
+Z= 1.8.^(-1.5*sqrt(X.^2 + Y.^2)).*cos(0.5.*Y).*sin(X);
 surf(X, Y, Z)
 labelPlot('Z=1.8^{(-1.5*sqrt(X^2 + Y^2))}*cos(0.5*Y)*sin(X)', 'x', 'y', 'z', true)
 
